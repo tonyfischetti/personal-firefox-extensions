ce22aa4523fa123f85213bdb3bcf875b68da254b
Tony Fischetti <tony.fischetti@gmail.com>
2017-12-09 12:02:19 -0500

    added wiki stuff and got rid of unecessary file

---
+++ remove-annoying-content/remove-wiki-sidebar.js
@@ -0,0 +1,6 @@
+
+var leftpanel = document.getElementById("mw-panel")
+leftpanel.style.visibility="hidden"
+var thecontent = document.getElementById("content")
+thecontent.style.margin="auto"
+thecontent.style.width="75%"
