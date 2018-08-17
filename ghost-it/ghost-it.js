862aaa3f9ae06022221bc923c424d186bf75de71
Tony Fischetti <tony.fischetti@gmail.com>
2018-08-17 10:31:37 -0400

    added the ability to remove (instead of hide) elements with Shift key

--- ghost-it/ghost-it.js
+++ ghost-it/ghost-it.js
@@ -5,23 +5,33 @@ var GODEEPER = "≠";
 var GOSHALLOWER = "–";
 var UNDO = "u";
 var STARTKEY = "Alt";
+var DESTROYELEMENT = "Shift";
 
 
 
 var thecurrentelement = "";
 var overthiselement = "";
 var overtheseelements = [];
-// var oldBackgroundColor = "";
 var ghostedElements = [];
+var destroyedElements = [];
 var howDeep = 1;
 var reddenedOldColors = [];
 var reddenedElements = [];
 
 var triggerKeyIsPressed = false;
+var destroyKeyIsPressed = false;
 
 
 window.addEventListener('click', function (e) {
-  if(triggerKeyIsPressed){
+  if(triggerKeyIsPressed && destroyKeyIsPressed){
+    elToRemove = reddenedElements[reddenedElements.length-1];
+    console.log("\ntrying to remove:");
+    console.log(elToRemove);
+    console.log("\n");
+    elToRemove.parentNode.removeChild(elToRemove);
+    // reddenedElements[0].parentNode.removeChild(reddenedElements[0]);
+    destroyedElements.push(elToRemove);
+  } else if(triggerKeyIsPressed){
     for(i = 0; i < reddenedElements.length; i++){
       var oldEl  = reddenedElements[i];
       var oldCol = reddenedOldColors[i];
@@ -33,6 +43,10 @@ window.addEventListener('click', function (e) {
 
 
 window.addEventListener('keydown', function(event) {
+  console.log(event.key);
+  console.log("\nreddened elements are now:");
+  console.log(reddenedElements);
+  console.log("\n");
   if(event.key===GODEEPER){
     if(triggerKeyIsPressed){
       howDeep = howDeep + 1;
@@ -69,9 +83,20 @@ window.addEventListener('keydown', function(event) {
     reddenedElements.push(overthiselement);
     overthiselement.style.backgroundColor = "red";
   }
+  if(event.key===DESTROYELEMENT){
+    destroyKeyIsPressed = true;
+    // overtheseelements = document.querySelectorAll(":hover");
+    // overthiselement = overtheseelements[overtheseelements.length-howDeep];
+    // var oldBackgroundColor = overthiselement.style.backgroundColor;
+    // reddenedOldColors.push(oldBackgroundColor);
+    // reddenedElements.push(overthiselement);
+  }
 });
 
 window.addEventListener('keyup', function(event) {
+  if(event.key===DESTROYELEMENT){
+    destroyKeyIsPressed = false;
+  }
   if(event.key===STARTKEY){
     triggerKeyIsPressed = false;
     for(i = 0; i < reddenedElements.length; i++){
